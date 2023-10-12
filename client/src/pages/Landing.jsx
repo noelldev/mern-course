e80e0cd1a693321bfdadd4d3519b9ea4bf3923a9
kodrr <ntlauigan@gmail.com>
2023-10-13 04:34:23 +0800

    Add content and style in Landing page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import styled from 'styled-components';
+import Wrapper from '../assets/wrappers/LandingPage';
+import main from '../assets/images/main.svg';
+import { Link } from 'react-router-dom';
+import { Logo } from '../components';
 
 const Landing = () => {
   return (
     <Wrapper>
-      <h1>Landing</h1>
-      <div className="content">some content</div>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Normcore JOMO pickled 3 wolf moon. Squid disrupt small batch
+            cardigan keytar distillery. Same jean shorts synth pitchfork,
+            selfies gentrify ramps knausgaard etsy single-origin coffee kogi
+            gluten-free. Thundercats echo park normcore kitsch prism. Yr
+            cardigan gorpcore enamel pin. Knausgaard palo santo ugh franzen
+            bicycle rights hella fingerstache cold-pressed kogi lo-fi bespoke
+            roof party
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
     </Wrapper>
   );
 };
-
-const Wrapper = styled.div`
-  background: red;
-  h1 {
-    color: white;
-  }
-  .content {
-    background: blue;
-    color: yellow;
-  }
-`;
 
 export default Landing;
